be4accf42c1e2f320e010fe3f627a2ad386890b4
Chris Warburton <chriswarbo@gmail.com>
2016-05-25 18:55:47 +0100

    Canonicalise functions

---
+++ canonical_functions.rkt
@@ -0,0 +1,85 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p racket -i racket
+#lang racket
+
+(require racket/include)
+(include "defs.rkt")
+
+(define given-functions
+  (map (lambda (line)
+         (with-input-from-string line
+           read))
+       (port->lines (current-input-port))))
+
+(define (normalise def)
+  (match def
+         [(list name args typ body) (mk-def name args typ (normalise-cases body))]
+         [_                         (error "Function definition of unexpected form")]))
+
+(define (normalise-cases exp)
+  (match exp
+         [(list 'case pat body) (list 'case
+                                      (replace-pat pat body)
+                                      (replace-case pat (normalise-cases body)))]
+         [(cons a b)            (cons (normalise-cases a) (normalise-cases b))]
+         [_                     exp]))
+
+(define (replace-pat pat body)
+  (match pat
+         [(cons con args) (cons con
+                                (get-normal-pat args (+ 1 (highest-var body))))]
+         [con             con]))
+
+(define (highest-var exp)
+  (if (string? exp)
+      (if (string-prefix? exp "var")
+          (string->number (substring exp 3))
+          -1)
+      (match exp
+             [(cons a b) (max (highest-var a) (highest-var b))]
+             [_          -1])))
+
+(define (replace-case pat body)
+  (match pat
+         [(list con)      body]
+         [(cons con args) (replace-args args (+ 1 (highest-var body)) body)]
+         [con             body]
+         [_               (error "Unexpected case structure" pat)]))
+
+(define (mk-def name args typ body)
+  (list name
+        (get-normal-args args 0)
+        typ
+        (replace-args (map car args) 0 body)))
+
+(define (nth-arg n)
+  (string-append "var" (number->string n)))
+
+(define (replace-args args n body)
+  (match args
+         [(cons arg rest) (replace-args rest
+                                        (+ 1 n)
+                                        (replace-in arg (nth-arg n) body))]
+         [null            body]))
+
+(define (replace-in src dst exp)
+  (if (equal? src exp)
+      dst
+      (match exp
+             [(cons a b) (cons (replace-in src dst a)
+                               (replace-in src dst b))]
+             [_          exp])))
+
+(define (get-normal-pat args n)
+  (match args
+         [(cons name rest) (cons (nth-arg n)
+                                 (get-normal-pat rest (+ 1 n)))]
+         [_                null]))
+
+(define (get-normal-args args n)
+  (match args
+         [(cons (list name typ) rest) (cons (list (nth-arg n) typ)
+                                            (get-normal-args rest (+ 1 n)))]
+         [_                           null]))
+
+(show (map normalise given-functions))
