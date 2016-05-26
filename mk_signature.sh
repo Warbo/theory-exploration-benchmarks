8609d51e34309a4af1aae9b5c7512eb969cc2440
Chris Warburton <chriswarbo@gmail.com>
2016-05-26 16:21:31 +0100

    Crude re-ordering

--- mk_signature.sh
+++ mk_signature.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#! /usr/bin/env nix-shell
+#! nix-shell -p haskellPackages.tip-lib -i bash
 
 # The symbols we're exploring in our signature
 TO_EXPLORE=$(cat | grep '^.')
@@ -31,4 +32,44 @@ TYPE_DEFS=$(echo "$TO_DEFINE" | ./type_def.sh)
 
 ALL_TYPES=$(echo -e "$CONS_DEFS\n$TYPE_DEFS" | sort -u)
 
-echo -e "$ALL_TYPES\n$FUNC_DEFS\n(check-sat)"
+UNORDERED_DEFS=$(echo -e "$ALL_TYPES\n$FUNC_DEFS")
+
+# Put definitions into dependency order
+
+function defOf {
+    # Get definition of $1 from stdin
+    GIVEN=$(cat)
+    echo "$GIVEN" | grep "(define-fun-rec $1 "
+}
+
+function nameOf {
+    # Get the names of the functions defined in stdin
+    GIVEN=$(cat)
+    echo "$GIVEN" | sed -e 's/(define-fun-rec \([^ ]*\) .*/\1/g'
+}
+
+function lessThanEq {
+    LTGIVEN=$(cat)
+    if echo "$LTGIVEN" | defOf "$1" | grep -F "$2" > /dev/null
+    then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function orderDefs {
+    GOT=$(cat)
+    FUNCS=$(echo "$GOT" | grep    "(define-fun")
+    TYPES=$(echo "$GOT" | grep -v "(define-fun")
+
+    # Types don't seem to have any dependencies
+    echo "$TYPES"
+
+    echo "$FUNCS" | grep    "(define-fun-rec equal"
+    echo "$FUNCS" | grep -v "(define-fun-rec equal"
+}
+
+DEFS=$(echo "$UNORDERED_DEFS" | orderDefs)
+
+tip <(echo -e "$DEFS\n(check-sat)") --haskell
