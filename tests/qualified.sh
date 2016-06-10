7de9eeea8c47aee36f50dbb882838a9651a37989
Chris Warburton <chriswarbo@gmail.com>
2016-06-10 14:08:05 +0100

    Ignore empty symbols in qualified.sh

--- tests/qualified.sh
+++ tests/qualified.sh
@@ -13,19 +13,37 @@ function report {
     fi
 }
 
+DIR="modules/tip-benchmarks/benchmarks"
+FRESH=$(find "$DIR" -name "*.smt2" | shuf | head -n10)
+
+REGRESSIONS="$DIR/tip2015/list_elem_map.smt2"
+
+FILES=$(echo -e "$REGRESSIONS\n$FRESH")
+COUNT=$(echo "$FILES" | wc -l)
+INDEX=1
+
+ALL_QUAL=1
 while read -r FILE
 do
+    echo "$INDEX/$COUNT" 1>&2
+    INDEX=$(( INDEX + 1 ))
+
     NAME=$(basename "$FILE")
     SYMS=$(NAME="$NAME" ./qualify.rkt < "$FILE" | ./symbols_of_theorems.rkt)
     QUALIFIED=1
     while read -r SYM
     do
         QUALIFIED=0
-    done < <(echo "$SYMS" | grep -vF "$NAME")
+        echo -e "Symbol '$SYM' unqualified in '$FILE'" 1>&2
+    done < <(echo "$SYMS" | grep -vF "$NAME" | grep '^.')
 
-    [[ "$QUALIFIED" -eq 1 ]]
-    report "$?" "All symbols in '$FILE' are qualified" ||
-        echo -e "SYMS:\n$SYMS\n\n" 1>&2
-done < <(find modules/tip-benchmarks/benchmarks -name "*.smt2" | shuf | head -n10)
+    [[ "$QUALIFIED" -eq 1 ]] || {
+        ALL_QUAL=0
+        echo -e "Symbols of '$FILE' not qualified\nSYMS:\n$SYMS\n\n" 1>&2
+    }
+done < <(echo "$FILES")
+
+[[ "$ALL_QUAL" -eq 1 ]]
+report "$?" "All symbols are qualified"
 
 exit "$ERR"
