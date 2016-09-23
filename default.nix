252af4d0f190fc34d9a05ce0aadd02f2f2d05a29
Chris Warburton <chriswarbo@gmail.com>
2016-09-23 14:00:40 +0100

    Augment Racket

--- default.nix
+++ default.nix
@@ -1,8 +1,9 @@
-{ bash, buildEnv, fetchurl, haskellPackages, makeWrapper, python, racket,
-  stdenv, writeScript }:
+{ bash, buildEnv, fetchurl, fetchFromGitHub, haskellPackages,
+  makeWrapper, python, racket, stdenv, writeScript }:
 
 let propagatedBuildInputs = [
-      bash haskellPackages.cabal-install python racket
+      bash haskellPackages.cabal-install python
+      (racketWithDeps [ shellPipeline ])
       (haskellPackages.ghcWithPackages (hs: [
         hs.tip-lib hs.QuickCheck hs.quickspec hs.testing-feat
       ])) ];
@@ -10,6 +11,53 @@ let propagatedBuildInputs = [
       name  = "tip-bench-env";
       paths = propagatedBuildInputs;
     };
+
+  shellPipeline = fetchFromGitHub {
+    owner  = "willghatch";
+    repo   = "racket-shell-pipeline";
+    rev    = "7ed9a75";
+    sha256 = "06z5bhmvpdhy4bakh30fzha4s0xp2arjq8h9cyi65b1y18cd148x";
+  };
+
+  racketWithDeps = deps: stdenv.mkDerivation {
+    name = "racket-with-deps";
+
+    buildInputs = [ makeWrapper racket ];
+    racketDeps  = deps;
+
+    buildCommand = ''
+      # raco writes to HOME, so make sure that's included
+      export HOME="$out/etc"
+      mkdir -p "$HOME"
+
+      # Each PKG should be a directory (e.g. pulled from git) containing
+      # "collections" as sub-directories. For example if PKG should allow
+      # (require utils/printing), it should contain PKG/utils/printing.rkt
+      mkdir -p "$out/share/pkgs"
+      for PKG in $racketDeps
+      do
+        # Make a mutable copy of the package, so we can compile it
+        NAME=$(basename "$PKG")
+        cp -r "$PKG" "$out/share/pkgs/$NAME"
+        chmod +w -R  "$out/share/pkgs/$NAME"
+
+        # raco is Racket's package manager, -D says "treat as a directory of
+        # collections", which is how git repos seem to be arranged.
+        raco link --user -D "$out/share/pkgs/$NAME"
+      done
+
+      # Compile packages
+      raco setup --avoid-main -x -D
+
+      # Provide Racket binaries patched to use our modified HOME
+      mkdir -p "$out/bin"
+      for PROG in "${racket}"/bin/*
+      do
+        NAME=$(basename "$PROG")
+        makeWrapper "$PROG" "$out/bin/$NAME" --set HOME "$out/etc"
+      done
+    '';
+  };
 in rec {
 
   # Scripts for combining all TIP benchmarks into one
@@ -64,16 +112,6 @@ in rec {
   tip-benchmark-smtlib = stdenv.mkDerivation {
     name        = "tip-benchmark-smtlib";
     buildInputs = [ te-benchmark ];
-
-    getSig = writeScript "tip-smtlib" ''
-               #!/usr/bin/env bash
-               set -e
-
-               BASE=$(dirname "$(readlink -f "$0")")
-               cd "$BASE/../lib/"
-
-               echo "$PWD/combined-benchmark.smt2"
-             '';
 
     teBenchmark = te-benchmark;
 
