9d64f593eeec63a60403e2448b5bd95702883b5d
Chris Warburton <chriswarbo@gmail.com>
2016-09-30 14:35:49 +0100

    Folded in more tests

--- defs.rkt
+++ defs.rkt
@@ -1756,6 +1756,43 @@
     '(1 3 5)))
 
 (module+ test
+  (define (names-match src data expect)
+    (define names (run-pipeline/out `(echo ,data)
+                                    `(./rec_names.rkt)))
+
+    (with-check-info
+     (('src     src)
+      ('data    data)
+      ('expect  expect)
+      ('names   names)
+      ('message "Got expected names"))
+    (check-equal? (string-trim names) (string-trim expect))))
+
+  (names-match "datatype"
+               "(declare-datatypes (a) ((list (nil) (cons (head a) (tail (list a))))))"
+               "list\nnil\ncons\nhead\ntail")
+
+  (names-match "function"
+               "(define-fun sort2 ((x Int) (y Int)) (list Int) (ite (<= x y) (cons x (cons y (as nil (list Int)))) (cons y (cons x (as nil (list Int))))))"
+               "sort2")
+
+  (names-match "parameterised function"
+               "(define-fun (par (a) (zsplitAt ((x Int) (y (list a))) (Pair (list a) (list a)) (Pair2 (ztake x y) (zdrop x y)))))"
+               "zsplitAt")
+
+  (names-match "recursive function"
+               "(define-fun-rec insert2 ((x Int) (y (list Int))) (list Int) (match y (case nil (cons x (as nil (list Int)))) (case (cons z xs) (ite (<= x z) (cons x y) (cons z (insert2 x xs))))))"
+               "insert2")
+
+  (names-match "recursive parameterised function"
+               "(define-fun-rec (par (a) (ztake ((x Int) (y (list a))) (list a) (ite (= x 0) (as nil (list a)) (match y (case nil (as nil (list a))) (case (cons z xs) (cons z (ztake (- x 1) xs))))))))"
+               "ztake")
+
+  (names-match "mutually recursive functions"
+               "(define-funs-rec ((stooge2sort2 ((x (list Int))) (list Int)) (stoogesort2 ((x (list Int))) (list Int)) (stooge2sort1 ((x (list Int))) (list Int))) ((match (zsplitAt (div (+ (* 2 (zlength x)) 1) 3) x) (case (Pair2 ys zs) (append (stoogesort2 ys) zs))) (match x (case nil (as nil (list Int))) (case (cons y z) (match z (case nil (cons y (as nil (list Int)))) (case (cons y2 x2) (match x2 (case nil (sort2 y y2)) (case (cons x3 x4) (stooge2sort2 (stooge2sort1 (stooge2sort2 x))))))))) (match (zsplitAt (div (zlength x) 3) x) (case (Pair2 ys zs) (append ys (stoogesort2 zs))))))"
+               "stooge2sort2\nstoogesort2\nstooge2sort1"))
+
+(module+ test
   (for-each (lambda (t)
               (test-case (string-append "Test script " t)
                 (in-temp-dir (lambda (d)
