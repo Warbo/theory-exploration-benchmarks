65770e8f03b3da1c4a6448af4444c71e140c7a0e
Chris Warburton <chriswarbo@gmail.com>
2017-01-23 10:27:02 +0000

    Allow sampling equations

--- scripts/choose_sample.rkt
+++ scripts/choose_sample.rkt
@@ -7,7 +7,8 @@
   (log "Usage: choose_sample <N> <M>\nWhere N and M are natural numbers.\nN \
 is the size of the sample and M is the index of the sample. Samples are chosen \
 deterministically (but unpredictably) using hashes, so M is required to get \
-multiple samples of the same size.\n")
+multiple samples of the same size.\n\nTo sample from equations only, set the \
+environment variable 'EQUATIONS_ONLY' to '1'.")
   (exit 1))
 
 (unless (equal? 2 (vector-length (current-command-line-arguments)))
@@ -21,7 +22,9 @@ multiple samples of the same size.\n")
   (usage))
 
 (define result
-  (sample-from-benchmarks size rep))
+  (if (equal? (getenv "EQUATIONS_ONLY") "1")
+      (sample-equational-from-benchmarks size rep)
+      (sample-from-benchmarks size rep)))
 
 (display (string-join (map symbol->string (set->list result))
                       "\n"))
