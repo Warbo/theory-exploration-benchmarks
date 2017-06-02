b2b0783450f77eb374d243f5abea7b3b4bd4fe81
Chris Warburton <chriswarbo@gmail.com>
2017-06-02 15:08:09 +0100

    Use precision/recall from sets not utils

--- scripts/lib/sets.rkt
+++ scripts/lib/sets.rkt
@@ -3,16 +3,17 @@
 ;; Functions for operating on sets (unordered lists with no duplicates)
 
 (require "util.rkt")
-(provide set-filter set-foldl set-map)
+(provide precision recall set-filter set-foldl set-map)
 
 (module+ test
-  (require rackunit)
   (require "testing.rkt"))
 
 ;; Map a function F over the elements of a set S. This shadows Racket's built-in
 ;; set-map, which unhelpfully returns a list (and has dodgy argument order)
 (define (set-map f s)
   (list->set (map f (set->list s))))
+
+
 
 (define/test-contract (precision found wanted)
   (-> set? set? rational?)
@@ -32,11 +33,11 @@
      (set-count wanted)))
 
 (module+ test
-  (test-case "Recall"
-             (check-equal? (/ 1 2)
-                           (recall (list->set '(a b c d e f g h i j k l m))
-                                   (list->set '(a b c d e f g h i j k l m
-                                                n o p q r s t u v w x y z))))))
+  (def-test-case "Recall"
+    (check-equal? (/ 1 2)
+                  (recall (list->set '(a b c d e f g h i j k l m))
+                          (list->set '(a b c d e f g h i j k l m
+                                         n o p q r s t u v w x y z))))))
 
 (define (set-filter f s)
   (list->set (filter f (set->list s))))
