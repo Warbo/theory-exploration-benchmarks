c9e19d2ad07668b0625de0c1c164c3ebb7eceeab
Chris Warburton <chriswarbo@gmail.com>
2018-08-13 12:30:00 +0100

    Make release.nix just build the current system

--- release.nix
+++ release.nix
@@ -1,33 +1,24 @@
-{ supportedSystems ? [ "i686-linux" "x86_64-linux" ] }:
-
 with builtins;
 with (import <nixpkgs> {}).lib;
+with import ./pkgs.nix {};
+with {
+  # Select the derivations we care about
+  parts = haskellPackages:
+    with callPackage ./. { inherit haskellPackages; };
+    {
+      # Useful infrastructure
+      inherit asv env scripts tools;
+      inherit (patchedHaskellPackages) tip-lib;
 
-genAttrs
-  supportedSystems
-  (system:
-    with import ./pkgs.nix { pkgsArgs = { inherit system; }; };
-    with {
-      # Select the derivations we care about
-      parts = haskellPackages:
-        with callPackage ./. {
-          inherit haskellPackages;
-          pkgsArgs = { inherit system; };
-        };
-        {
-          # Useful infrastructure
-          inherit asv env scripts tools;
-          inherit (patchedHaskellPackages) tip-lib;
+      # Benchmark data
+      inherit tip-benchmark-haskell tip-benchmark-smtlib;
 
-          # Benchmark data
-          inherit tip-benchmark-haskell tip-benchmark-smtlib;
-
-          # Tests
-          quickTests = tests { full = false; };
-          fullTests  = tests { full = true;  };
-        };
+      # Tests
+      quickTests = tests { full = false; };
+      fullTests  = tests { full = true;  };
     };
-    # Add more versions to this list if we can get them to work
-    mapAttrs (_: parts)
-             (filterAttrs (n: _: elem n [ "ghc7103" "ghc801" "ghc802" ])
-                          haskell.packages))
+};
+# Add more versions to this list if we can get them to work
+mapAttrs (_: parts)
+         (filterAttrs (n: _: elem n [ "ghc7103" "ghc801" "ghc802" ])
+                      haskell.packages)
